a8eda9fc30af4e3ef4f76fe3db0e0c4ff82074c2
Kappaccinoh <lim.jia28@gmail.com>
2023-10-24 22:18:17 +0800

    mysql integration

---
+++ main-API/README.md
@@ -0,0 +1,2 @@
+# MAIN-API For Orange House Bot
+Backend API for Orange House Bot
--- main-API/index.js
+++ main-API/index.js
@@ -1,4 +1,20 @@
-const app = require('express')();
-const PORT = 
+const express = require('express');
+const app = express();
+const PORT = 8080;
 
-app.listen()
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: '',
+  user: '',
+  password: '',
+  database: ''
+});
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+    if (err) throw err;
+    console.log('The solution is: ', rows[0].solution);
+});
+  
+app.listen();
