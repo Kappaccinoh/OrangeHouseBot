bacd31dffe5178527c3ed02a853b073171a30580
Kappaccinoh <lim.jia28@gmail.com>
2023-10-24 22:54:18 +0800

    alter vars

--- main-API/utils/database.js
+++ main-API/utils/database.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: process.env.DBPASSWORD,
-  database: process.env.DBNAME
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
