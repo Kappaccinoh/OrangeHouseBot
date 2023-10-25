b54635c4146c492fe90e600b5acc1cc9e139c64e
Kappaccinoh <lim.jia28@gmail.com>
2023-10-25 08:13:55 +0800

    removed credentials

--- main-API/utils/database.js
+++ main-API/utils/database.js
@@ -4,14 +4,15 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 connection.connect((err) => {
     if (!err) {
       console.log("Connected");
     } else {
-      console.log("Connection Failed");
+      throw err;
     }
 });
 
