b7523a28b85a2b6804d0d68658483d3723b67793
Kappaccinoh <lim.jia28@gmail.com>
2023-10-26 23:58:08 +0800

    working convo logic

--- main-tele/bot.py
+++ main-tele/bot.py
@@ -20,68 +20,81 @@ logging.basicConfig(
     level=logging.INFO
 )
 
+CREATEPOLL = range(1)
+
 # Bot Admin Commands
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    buttons = [
-        [KeyboardButton('Poll')],
-        [KeyboardButton('Help')]
-    ]
     text = \
         'HELLOO THEREE! My name is Soyaya, I am the mascot for Orange House! NICE TO MEET EVERYONE!\n\n' \
         'How may I assist yall today?' \
         'Type in /help for my full list of commands.'
 
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text, reply_markup=ReplyKeyboardMarkup(buttons))
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    buttons = [
-        [KeyboardButton('Poll')],
-        [KeyboardButton('Help')]
-    ]
-
     text = \
-        'Poll - Allows you to create a poll in which people can join'
+        '/poll - Allows you to create a poll in which people can join'
 
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text, reply_markup=ReplyKeyboardMarkup(buttons))
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 
-# Function which responds to KeyboardMarkup Options, triggers the specific set of the bot's capabilities
-async def message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    message = update.message
-    text = message.text
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    await update.message.reply_text(
+        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
+    )
 
-    if text == 'Create Poll':
-        await poll(update, context)
-    elif text == 'Help':
-        await help(update, context)
-    else:
-        await start(update, context)
+    return ConversationHandler.END
 
 
 # Polling Functions
+'''
+/createpoll <title>, takes in one argument which is the title of the poll
+'''
 async def poll(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    buttons = [
-        [KeyboardButton('Create Poll')],
-        [KeyboardButton('Join')],
-        [KeyboardButton('Remove')]
-        [KeyboardButton('Delete Poll')]
-        [KeyboardButton('Help')]
-    ]
+    await update.message.reply_text(text='Enter the title of your Poll')
+    
+    return CREATEPOLL
 
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text, reply_markup=ReplyKeyboardMarkup(buttons))
+async def createpoll(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    title = update.message.text
+
+    print(title)
+
+    return ConversationHandler.END
+
+
+async def deletepoll(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    message = update.message
+    text = message.text
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="deletepoll")
+
+
 
 # Main Driver Code
 if __name__ == '__main__':
     application = ApplicationBuilder().token(BOT_TOKEN).build()
     
-    start_handler = CommandHandler('start', start)
+    start_handler = CommandHandler("start", start)
+    help_handler = CommandHandler("help", help)
     unknown_handler = MessageHandler(filters.COMMAND, unknown)
-    message_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), message)
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("poll", poll)],
+        states={
+            CREATEPOLL: [MessageHandler(filters.TEXT, createpoll)],
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
+    )
+    application.add_handler(conv_handler)
 
     application.add_handler(start_handler)
-    application.add_handler(message_handler)
+    application.add_handler(help_handler)
     application.add_handler(unknown_handler)
 
     application.run_polling()
