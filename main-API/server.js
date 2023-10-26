95fb469a5913b47eb5845017d1ccd209e897028a
Kappaccinoh <lim.jia28@gmail.com>
2023-10-26 20:54:12 +0800

    added python logic

--- main-API/server.js
+++ main-API/server.js
@@ -6,21 +6,28 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.json());
 
-// const Poll = require
+// Landing Page for Bot
+app.get('/', (req, res) => {
+  res.send(
+    'OrangeHouseBot API'
+  )
+})
+
+app.get('/help', (req, res) => {
+  res.send(
+    "/create <title> - Creates a new and empty list\n" +
+    "/join <your_name> <room_number> - Joins and or updates the current list (type /join again to override your previous entry)\n" +
+    "/remove - Removes your current entry\n" +
+    "/end - Deletes the current list of names"
+  )
+})
+
 
 // Logic for Polling Bot Start
 
-// "HELLOO THEREE! My name is Soyaya, I am the mascot for Orange House! NICE TO MEET EVERYONE!\n\n" +
-// "How may I assist yall today? Type in /help for my full list of commands."
-
-// "/create <title> - Creates a new and empty list\n" +
-//     "/join <your_name> <room_number> - Joins and or updates the current list (type /join again to override your previous entry)\n" +
-//     "/remove - Removes your current entry\n" +
-// "/end - Deletes the current list of names")
-
-app.get('/', (req, res) => {
+app.get('/poll', (req, res) => {
   mysql.query(
-    "SELECT * FROM new_table",
+    "SELECT * FROM PollTable",
     (err, results, fields) => {
       if (!err) {
         res.send(results);
@@ -30,6 +37,19 @@ app.get('/', (req, res) => {
     }
   );
 })
+
+app.post('/poll/create', async(req, res) => {
+  try {
+    const 
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({message:e.message})
+  }
+});
+
+
+
+
 
 // Logic for Polling Bot End
 
--- main-tele/bot.py
+++ main-tele/bot.py
@@ -1,7 +1,88 @@
+from dotenv import load_dotenv
 import os
-import telebot
-import re
+import logging
+import telegram
+from telegram import *
+from telegram.ext import *
+import requests
+import json
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
 
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
-# BOT_TOKEN = ""
-bot = telebot.TeleBot(BOT_TOKEN)
+api_url = 'https://laundrobot-api.onrender.com'
+api_url = 'http://localhost:4000'
+
+load_dotenv()
+BOT_TOKEN = os.environ['BOT_TOKEN']
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+# Bot Admin Commands
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    buttons = [
+        [KeyboardButton('Poll')],
+        [KeyboardButton('Help')]
+    ]
+    text = \
+        'HELLOO THEREE! My name is Soyaya, I am the mascot for Orange House! NICE TO MEET EVERYONE!\n\n' \
+        'How may I assist yall today?' \
+        'Type in /help for my full list of commands.'
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text, reply_markup=ReplyKeyboardMarkup(buttons))
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    buttons = [
+        [KeyboardButton('Poll')],
+        [KeyboardButton('Help')]
+    ]
+
+    text = \
+        'Poll - Allows you to create a poll in which people can join'
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text, reply_markup=ReplyKeyboardMarkup(buttons))
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+# Function which responds to KeyboardMarkup Options, triggers the specific set of the bot's capabilities
+async def message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message = update.message
+    text = message.text
+
+    if text == 'Create Poll':
+        await poll(update, context)
+    elif text == 'Help':
+        await help(update, context)
+    else:
+        await start(update, context)
+
+
+# Polling Functions
+async def poll(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    buttons = [
+        [KeyboardButton('Create Poll')],
+        [KeyboardButton('Join')],
+        [KeyboardButton('Remove')]
+        [KeyboardButton('Delete Poll')]
+        [KeyboardButton('Help')]
+    ]
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text, reply_markup=ReplyKeyboardMarkup(buttons))
+
+# Main Driver Code
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
+    
+    start_handler = CommandHandler('start', start)
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+    message_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), message)
+
+    application.add_handler(start_handler)
+    application.add_handler(message_handler)
+    application.add_handler(unknown_handler)
+
+    application.run_polling()
+    
