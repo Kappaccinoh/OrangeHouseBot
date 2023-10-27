a6645926cb199b5d51d571bf95f7aaed45009c48
Kappaccinoh <lim.jia28@gmail.com>
2023-10-27 14:24:30 +0800

    more routing logic

--- main-API/server.js
+++ main-API/server.js
@@ -1,7 +1,7 @@
 const mysql = require('./utils/database');
 const express = require('express');
 const bodyParser = require("body-parser");
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
@@ -25,29 +25,49 @@ app.get('/help', (req, res) => {
 
 // Logic for Polling Bot Start
 
-app.get('/poll', (req, res) => {
-  mysql.query(
-    "SELECT * FROM PollTable",
-    (err, results, fields) => {
-      if (!err) {
-        res.send(results);
-      } else {
-        console.log(err);
-      }
-    }
-  );
+app.get('/poll', async (req, res) => {
+  try {
+    sqlQuery = `SELECT * FROM ${process.env.DB_TABLE_NAME} WHERE chatid = ${req.query['chatid']}`;
+    const result = await mysql.query(sqlQuery);
+    res.status(200).json(result)
+  } catch (e) {
+    console.log(e.message);
+    return res.status(500).json({message:e.message});
+  }
 })
 
 app.post('/poll/create', async(req, res) => {
   try {
-    sqlQuery = `INSERT INTO ${process.env.DB_TABLE_NAME} (name, room, telehandle, chatid) VALUES (${req.query['name']}, ${req.query['room']}, ${req.query['telehandle']}, ${req.query['chatid']})`;
-    const result = await mysql
-    .query(sqlQuery)
+    sqlQuery = `INSERT INTO ${process.env.DB_TABLE_NAME2} (chatid, polltitle) VALUES (${req.query['chatid']}, ${req.query['polltitle']})`;
+    const result = await mysql.query(sqlQuery)
+    res.status(200).json(result);
   } catch (e) {
     console.log(e.message);
     return res.status(500).json({message:e.message});
   }
 });
+
+app.post('/poll/join', async(req, res) => {
+  try {
+    sqlQuery = `INSERT INTO ${process.env.DB_TABLE_NAME} (name, room, telehandle, chatid) VALUES (${req.query['name']}, ${req.query['room']}, ${req.query['telehandle']}, ${req.query['chatid']})`;
+    const result = await mysql.query(sqlQuery)
+    res.status(200).json(result);
+  } catch (e) {
+    console.log(e.message);
+    return res.status(500).json({message:e.message});
+  }
+});
+
+app.delete('/poll/delete', async(req, res) => {
+  try {
+    sqlQuery = `DELETE FROM ${process.env.DB_TABLE_NAME2} WHERE chatid = ${req.query['chatid']}`;
+    const result = await mysql.query(sqlQuery)
+    res.status(200).json(result);
+  } catch (e) {
+    console.log(e.message);
+    return res.status(500).json({message:e.message});
+  }
+})
 
 // Logic for Polling Bot End
 
